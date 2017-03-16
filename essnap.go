99c46d458cc8226648fe6f6c612bbf63781bae34
Philip Hutchins <flipture@gmail.com>
2017-03-16 11:26:02 -0400

    Watching free and used space on all nodes in a cluster from one node now

--- essnap.go
+++ essnap.go
@@ -20,9 +20,48 @@ type IndexItem struct {
   PriStoreSize string `json:"pri.store"`
 }
 
-type IndexList struct {
-  Collection []IndexItem
+type NodeStats struct {
+  NodesInfo NodeInfo
+  ClusterName string `json:"cluster_name"`
+  Nodes map[string]Node
 }
+
+type Node struct {
+  Timestamp int
+  Name string
+  FS FS
+}
+
+type FS struct {
+  Timestamp int
+  Total FSTotals
+}
+
+type FSTotals struct {
+  TotalInBytes int `json:"total_in_bytes"`
+  FreeInBytes int `json:"free_in_bytes"`
+  AvailableInBytes int `json:"available_in_bytes"`
+}
+
+type NodeInfo struct {
+  Total int
+  Successful int
+  Failed int
+}
+
+type Uri struct {
+  Proto string
+  Path string
+  Settings string
+}
+
+type ApiResponse struct {
+  Body string
+}
+
+// Get the response.body from the server and return that in some kind of generic struct
+
+// Add a decode method on the struct that returns json given a new struct
 
 func GetIndicesList() []IndexItem {
   indexList := []IndexItem{}
@@ -55,4 +94,37 @@ func GetIndicesList() []IndexItem {
   }
 
   return indexList
+}
+
+func GetNodeStats() NodeStats {
+  nodeStats := NodeStats{}
+
+  myConfig := config.New("config.json")
+
+  protocol := "http://"
+  httpPath := "/_nodes/stats/fs"
+  settings := "?format=json&pretty"
+
+  requestURI := protocol + myConfig.ESHost + httpPath + settings
+
+  resp, err := http.Get(requestURI)
+
+  if err != nil {
+    fmt.Println("Error getting index list: ", err)
+  } else {
+    defer resp.Body.Close()
+
+    decoder := json.NewDecoder(resp.Body)
+    err := decoder.Decode(&nodeStats)
+
+    if err != nil {
+      fmt.Println("Error decoding JSON: ", err)
+    }
+
+    // Example for printing the JSON
+    //out, _ := json.Marshal(nodeStats)
+    //fmt.Println("Out", string(out))
+  }
+
+  return nodeStats
 }
--- main.go
+++ main.go
@@ -2,23 +2,87 @@ package main
 
 import (
   "fmt"
-  "es-archivist/config"
+//  "encoding/json"
+  "time"
 )
 
-func main() {
-  myConfig := config.New("config.json")
+type NodesFSData struct {
+  Name string
+  Totals FSTotals
+}
 
-  fmt.Println("Got config, ES Host is: " + myConfig.ESHost)
+func main() {
+  //myConfig := config.New("config.json")
+
+  //fmt.Println("Got config, ES Host is: " + myConfig.ESHost)
 
   // Get the list of all indices
-  indexList := GetIndicesList()
-
-  fmt.Println("First element: " + indexList[0].Health)
+  //indexList := GetIndicesList()
+  //fmt.Println("First element: " + indexList[0].Health)
 
   // Create a date ordered array of indices
-  indexArray := getIndexArray(indexList)
+  //indexArray := getIndexArray(indexList)
+  //fmt.Println("Index array: ", indexArray)
 
-  fmt.Println("Index array: ", indexArray)
+
+  // Watch the storage space left on each of the nodes
+  watchStorageSpace()
+}
+
+func watchStorageSpace() {
+  for {
+    nodeStats := GetNodeStats()
+    // Get current node stats
+
+    //out, _ := json.Marshal(nodeStats)
+    //fmt.Printf("nodeStats: %v", string(out))
+
+    var nodeFSDataArray []NodesFSData
+
+    for key, element := range nodeStats.Nodes {
+      var fsData NodesFSData
+
+      fsData.Name = key
+      fsData.Totals = element.FS.Total
+
+      //data, _ := json.Marshal(element.FS.Total)
+
+      //fmt.Printf("Adding %+v to data\n", string(data))
+      nodeFSDataArray = append(nodeFSDataArray, fsData)
+    }
+
+    for _, node := range nodeFSDataArray {
+      fmt.Printf("Node '%s' has %v free space left out of %v\n", node.Name, node.Totals.FreeInBytes, node.Totals.TotalInBytes)
+    }
+
+    // If storage space drops below specified level, kick off a snapshot
+    //   of the oldest index
+
+    // Take a snapshot one (or more) indices at a time
+    initialResponse := snapshotOldestIndex()
+
+    // If initialResponse is not OK, do something, report the failure?
+    if initialResponse != "accepted" {
+    }
+
+    // Wait for the snapshot(s) to complete
+
+    // Ensure that the snapshot was successful
+    // If it was not, delete the failed or partial attempt and retry
+
+    // When completed, delete the indices that were snapshotted
+
+    // Wait some period of time for the disk usage to stabalize
+    // Then continue to watch disk usage
+
+    fmt.Println("Sleeping...")
+    time.Sleep(5000 * time.Millisecond)
+  }
+}
+
+func snapshotOldestIndex() string {
+
+  return "stub no error"
 }
 
 func getIndexArray(il []IndexItem) []string {
